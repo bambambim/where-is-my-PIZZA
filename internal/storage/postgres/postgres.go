cc2a09133471120d517e2bae98ca54260f0e74e8
bambambim <dan4ik2006@mail.ru>
2025-09-18 17:33:55 +0500

    feat: postgres init

--- internal/storage/postgres/postgres.go
+++ internal/storage/postgres/postgres.go
@@ -1,1 +1,26 @@
 package postgres
+
+import "database/sql"
+
+type Repo struct {
+	db *sql.DB
+}
+
+func Connect(dburl string) (*sql.DB, error) {
+	db, err := sql.Open("postgres", dburl)
+	if err != nil {
+		return nil, err
+		//log
+	}
+	err = db.Ping()
+	if err != nil {
+		return nil, err
+		//log
+	}
+
+	return db, nil
+}
+
+func New(db *sql.DB) *Repo {
+	return &Repo{db: db}
+}
