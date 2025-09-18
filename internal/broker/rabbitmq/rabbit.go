2a8cf00da97b2831502ee28a8b86059702ffb74c
bambambim <dan4ik2006@mail.ru>
2025-09-18 17:33:39 +0500

    feat: rabbit init

--- internal/broker/rabbitmq/rabbit.go
+++ internal/broker/rabbitmq/rabbit.go
@@ -1,1 +1,30 @@
 package rabbitmq
+
+import "github.com/rabbitmq/amqp091-go"
+
+type Rabbit struct {
+	conn *amqp091.Connection
+	ch   *amqp091.Channel
+}
+
+func Connect(rabbitUrl string) (*amqp091.Connection, error) {
+	conn, err := amqp091.Dial(rabbitUrl)
+	if err != nil {
+		return nil, err
+		//log
+	}
+	return conn, nil
+}
+
+func CreateChannel(conn *amqp091.Connection) (*amqp091.Channel, error) {
+	ch, err := conn.Channel()
+	if err != nil {
+		return nil, err
+		//log
+	}
+	return ch, nil
+}
+
+func New(conn *amqp091.Connection, ch *amqp091.Channel) *Rabbit {
+	return &Rabbit{conn: conn, ch: ch}
+}
